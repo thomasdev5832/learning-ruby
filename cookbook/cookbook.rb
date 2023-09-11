b5f5bd460f58421c58cbf7c6f4a2bc0591f52fc2
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-11 16:08:19 -0300

    ruby cookbook

---
+++ cookbook/cookbook.rb
@@ -0,0 +1,6 @@
+puts "Welcome to the Cookbook"
+
+puts "What is the name of the recipe?"
+recipe_name = gets.chomp()
+# String interpolation
+puts "Recipe - #{recipe_name} - registered successfully!"
